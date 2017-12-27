fa5358e05a7eda900ffad7e43296086f5696fe4e
phuongthanh96 <phuongthanhpham1996@gmail.com>
2017-12-27 18:27:40 +0700

    add jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
+//= require jquery/dist/jquery
 //= require bootstrap
 //= require rails-ujs
 //= require turbolinks
