09033ec2708319853644f2581fef7cc4929614a5
phuongthanh96 <phuongthanhpham1996@gmail.com>
2018-01-03 20:45:52 +0700

    config host server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,8 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  host = 'thanhpp.herokuapp.com'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
 end
